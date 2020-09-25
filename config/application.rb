3c533bd8c22937ffb2ef5a690257545c49cc3e60
kazuki17 <71236928+kazuki17@users.noreply.github.com>
2020-09-25 21:00:30 +0900

    Active Storageの導入

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,9 @@ module ChatApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.i18n.default_locate = :ja
+    config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -22,8 +23,5 @@ module ChatApp
       g.helper false
       g.test_framework false
     end
-    config.i18n.default_locale = :ja
-    config.time_zone = 'Tokyo'
-    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
   end
 end
